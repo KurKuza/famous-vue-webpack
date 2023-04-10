eadd07e96edc5d193eb72301ba76b35802986416
Kurkuza <danilsherrbakov1993@gmail.com>
2023-04-10 17:36:06 +0300

    added request by url

--- src/components/products/card/CardButton.vue
+++ src/components/products/card/CardButton.vue
@@ -1,19 +1,33 @@
 <script setup>
 import { successIcon } from '@/assets/icons'
 import Button from '@/components/buttons/Button.vue'
+import { ref } from 'vue'
+import axios from 'axios'
 const props = defineProps({
 	isInTheBasket: {
 		type: Boolean,
 		default: false,
 	},
 })
+
+const isInTheBasket = ref(props.isInTheBasket)
+
+async function addInBasket() {
+	isInTheBasket.value = !isInTheBasket.value
+	const res = await axios.get('https://dummyjson.com/products')
+	const data = res.data
+
+	console.log(data)
+}
 </script>
 
 <template>
-	<Button v-if="isInTheBasket" buttonAccentColor3 :icon="successIcon"
-		>В корзине</Button
-	>
-	<Button v-else buttonAccentColor2>Купить</Button>
+	<div @click="addInBasket">
+		<Button v-if="isInTheBasket" buttonAccentColor3 :icon="successIcon"
+			>В корзине</Button
+		>
+		<Button v-else buttonAccentColor2>Купить</Button>
+	</div>
 </template>
 
 <style lang="scss"></style>
