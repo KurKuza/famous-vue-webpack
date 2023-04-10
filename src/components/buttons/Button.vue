1237e010f9d80f1fb4b5396b55e1c0ee7f25db9f
Kurkuza <danilsherrbakov1993@gmail.com>
2023-04-10 21:55:30 +0300

    Added loader

--- src/components/buttons/Button.vue
+++ src/components/buttons/Button.vue
@@ -1,9 +1,11 @@
 <script setup>
+import Loader from '@/components/Loader.vue'
 const props = defineProps({
 	buttonAccentColor1: { type: Boolean, required: false },
 	buttonAccentColor2: { type: Boolean, required: false },
 	buttonAccentColor3: { type: Boolean, required: false },
 	icon: { type: String, required: false },
+	loader: { type: Boolean, required: false },
 })
 </script>
 
@@ -11,13 +13,16 @@ const props = defineProps({
 	<button
 		class="button"
 		:class="{
-			button__accent_color_1: props.buttonAccentColor1,
-			button__accent_color_2: props.buttonAccentColor2,
-			button__accent_color_3: props.buttonAccentColor3,
+			button__accent_1: props.buttonAccentColor1,
+			button__accent_2: props.buttonAccentColor2,
+			button__accent_3: props.buttonAccentColor3,
 		}"
 	>
-		<img v-if="icon" class="button__icon" :src="icon" />
-		<slot />
+		<Loader v-if="true" class="button__loader" />
+		<div v-else class="button__content">
+			<img v-if="icon" class="button__icon" :src="icon" />
+			<slot />
+		</div>
 	</button>
 </template>
 
@@ -30,17 +35,23 @@ const props = defineProps({
 	font-weight: 700;
 	height: 3rem;
 	width: 7rem;
-}
-.button__icon {
-	margin-right: 0.25rem;
-}
-.button__accent_color_1 {
-	background-color: var(--accentColor1);
-}
-.button__accent_color_2 {
-	background-color: var(--accentColor2);
-}
-.button__accent_color_3 {
-	background-color: var(--accentColor3);
+	&__content {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+	&__icon {
+		margin-right: 0.25rem;
+	}
+
+	&__accent_1 {
+		background-color: var(--accentColor1);
+	}
+	&__accent_2 {
+		background-color: var(--accentColor2);
+	}
+	&__accent_3 {
+		background-color: var(--accentColor3);
+	}
 }
 </style>
