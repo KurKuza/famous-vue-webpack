32f30f07a34676b92ae0e969a8419134418ac0a9
Kurkuza <danilsherrbakov1993@gmail.com>
2023-04-13 23:47:18 +0300

    added links in footer

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,2 +1,4 @@
 export { default as successIcon } from './success.svg'
 export { default as logoFamous } from './logo-famous.svg'
+export { default as phone } from './phone.svg'
+export { default as address } from './address.svg'
--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,4 +1,5 @@
 <script setup>
+import { phone, address } from '@/assets/icons'
 import Logo from '@/components/Logo.vue'
 import Container from '@/components/Container.vue'
 import PageNavigator from '@/components/PageNavigator.vue'
@@ -11,6 +12,20 @@ import Search from '@/components/Search.vue'
 			<div class="footer__left">
 				<Logo dark />
 				<PageNavigator />
+			</div>
+			<div class="footer__right">
+				<div class="footer__phone">
+					<img :src="phone" />
+					<a href="tel:+7(495)555-55-55">+7 (495) 555-55-55</a>
+				</div>
+				<div class="footer__address">
+					<img :src="address" />
+					<a
+						target="_blank"
+						href="https://yandex.ru/maps/213/moscow/house/ulitsa_raspletina_24/Z04Ycg9kS00HQFtvfXt4dXhmYw==/?ll=37.484280%2C55.794457&z=17.2"
+						>г. Москва, ул. Расплетина, 24</a
+					>
+				</div>
 			</div>
 		</Container>
 	</footer>
@@ -32,6 +47,24 @@ import Search from '@/components/Search.vue'
 		justify-content: center;
 		align-items: center;
 		gap: 3rem;
+	}
+	&__right {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		gap: 3rem;
+	}
+	&__phone {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		gap: 0.5rem;
+	}
+	&__address {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		gap: 0.5rem;
 	}
 }
 </style>
